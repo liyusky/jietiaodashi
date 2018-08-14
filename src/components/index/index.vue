2339cf11bae99f4fe035343bb36739a45d6808f8
“CZHub <1979250385@qq.com>
2018-08-14 16:22:06 +0800

    xxxxx

---
+++ src/components/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- s  -->
+  <section class="index"></section>
+  <!-- e  -->
+</template>
+
+<script>
+export default {
+  name: 'IndexComponent',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "./index.scss";
+</style>
