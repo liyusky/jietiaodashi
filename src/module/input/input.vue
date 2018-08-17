18d77cb7b682830ccb0fcac8c2422ed83550074d
“CZHub <1979250385@qq.com>
2018-08-17 14:11:18 +0800

    refresh

--- src/module/input/input.vue
+++ src/module/input/input.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- s  -->
   <section class="input">
-    <div class="input-default">
-      <input v-model="inputText" type="text" placeholder="输入内容">
+    <div class="input-default" v-if="input.type === 'default'">
+      <input v-model="inputText" type="text" :placeholder="text.placeholder">
       <i class="iconfont" :class="input.icon" v-show="input.icon"></i>
     </div>
-    <div class="input-text">
+    <div class="input-text" v-if="input.type === 'text'">
       <span>手机号</span>
-      <input type="text" placeholder="输入手机号">
+      <input type="text" :placeholder="text.placeholder">
     </div>
   </section>
   <!-- e  -->
@@ -16,6 +16,7 @@
 <script>
 export default {
   name: 'InputComponent',
+  props: ['input'],
   data() {
     return {
       inputText: '',
