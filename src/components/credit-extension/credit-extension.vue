a72c26e868015a6142d5df6c0425dce9eb3923bd
“CZHub <1979250385@qq.com>
2018-08-31 15:08:40 +0800

    修改授信页面

--- src/components/credit-extension/credit-extension.vue
+++ src/components/credit-extension/credit-extension.vue
@@ -13,54 +13,69 @@
       </div>
       <div class="content-step padding-horizontal-21">
         <div class="step-item bg-write margin-bottom-21">
-          <svg class="icon" aria-hidden="true">
+          <svg class="icon" aria-hidden="true" v-if="identityShow">
             <use xlink:href="#icon-cong"></use>
+          </svg>
+          <svg class="icon" aria-hidden="true" v-if="!identityShow">
+            <use xlink:href="#icon-cangjian"></use>
           </svg>
           <div class="item-center">
             <p class="font-30 color-black">身份认证</p>
             <p class="font-24 color-black color-light-grey">上传你的真实身份信息</p>
           </div>
-          <button class="item-button button bg-blue color-white font-24"><div>去认证</div></button>
+          <button class="item-button button bg-blue color-white font-24" :class="{'button-active': identityShow}" @click="toAttestation"><div>{{identityShow ? '已完成' : '去认证'}}</div></button>
         </div>
         <div class="step-item bg-write margin-bottom-21">
-          <svg class="icon" aria-hidden="true">
+          <svg class="icon" aria-hidden="true" v-if="contactShow">
             <use xlink:href="#icon-cong"></use>
+          </svg>
+          <svg class="icon" aria-hidden="true" v-if="!contactShow">
+            <use xlink:href="#icon-cangjian"></use>
           </svg>
           <div class="item-center">
             <p class="font-30 color-black">联系人认证</p>
             <p class="font-24 color-black color-light-grey">补全联系人资料，获取更高额度</p>
           </div>
-          <button class="item-button button bg-blue color-white font-24"><div>去认证</div></button>
+          <button class="item-button button bg-blue color-white font-24"><div>{{identityShow ? '已完成' : '去认证'}}</div></button>
         </div>
         <div class="step-item bg-write margin-bottom-21">
-          <svg class="icon" aria-hidden="true">
+          <svg class="icon" aria-hidden="true" v-if="sesameShow">
             <use xlink:href="#icon-cong"></use>
+          </svg>
+          <svg class="icon" aria-hidden="true" v-if="!sesameShow">
+            <use xlink:href="#icon-cangjian"></use>
           </svg>
           <div class="item-center">
             <p class="font-30 color-black">芝麻分认证</p>
             <p class="font-24 color-black color-light-grey">做信用达人，晒晒芝麻信用分吧</p>
           </div>
-          <button class="item-button button bg-blue color-white font-24"><div>去认证</div></button>
+          <button class="item-button button bg-blue color-white font-24"><div>{{identityShow ? '已完成' : '去认证'}}</div></button>
         </div>
         <div class="step-item bg-write margin-bottom-21">
-          <svg class="icon" aria-hidden="true">
+          <svg class="icon" aria-hidden="true" v-if="operatorShow">
             <use xlink:href="#icon-cong"></use>
+          </svg>
+          <svg class="icon" aria-hidden="true" v-if="!operatorShow">
+            <use xlink:href="#icon-cangjian"></use>
           </svg>
           <div class="item-center">
             <p class="font-30 color-black">运营商认证</p>
             <p class="font-24 color-black color-light-grey">最后一步授权运行商查询</p>
           </div>
-          <button class="item-button button bg-blue color-white font-24"><div>去认证</div></button>
+          <button class="item-button button bg-blue color-white font-24"><div>{{identityShow ? '已完成' : '去认证'}}</div></button>
         </div>
         <div class="step-item bg-write">
-          <svg class="icon" aria-hidden="true">
+          <svg class="icon" aria-hidden="true" v-if="cardShow">
             <use xlink:href="#icon-cong"></use>
+          </svg>
+          <svg class="icon" aria-hidden="true" v-if="!cardShow">
+            <use xlink:href="#icon-cangjian"></use>
           </svg>
           <div class="item-center">
             <p class="font-30 color-black">银行卡认证</p>
             <p class="font-24 color-black color-light-grey">添加银行卡</p>
           </div>
-          <button class="item-button button bg-blue color-white font-24"><div>去认证</div></button>
+          <button class="item-button button bg-blue color-white font-24"><div>{{identityShow ? '已完成' : '去认证'}}</div></button>
         </div>
       </div>
     </div>
@@ -75,6 +90,12 @@ export default {
   name: 'CreditExtensionComponent',
   data () {
     return {
+      iconShow: false,
+      identityShow: false,
+      contactShow: false,
+      sesameShow: false,
+      operatorShow: false,
+      cardShow: false,
       // start params
       'title': {
         contentText: '授信'
@@ -87,6 +108,9 @@ export default {
     // include components
   },
   methods: {
+    toAttestation () {
+      this.identityShow = true
+    },
     backPage () {
       this.$router.back(-1)
     }
