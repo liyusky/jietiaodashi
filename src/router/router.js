7dd0a6393eda893dedd2c8e07c737ae9451cc596
“CZHub <1979250385@qq.com>
2018-09-06 18:12:46 +0800

    refresh

--- src/router/router.js
+++ src/router/router.js
@@ -31,7 +31,7 @@ const MessageComponent = () => import(/* webpackChunkName: 'message' */ '../comp
 const MineComponent = () => import(/* webpackChunkName: 'mine' */ '../components/home/mine/mine.vue')
 const WannaBorrowComponent = () => import(/* webpackChunkName: 'wanna-borrow' */ '../components/home/wanna-borrow/wanna-borrow.vue')
 const IdentityVerificationComponent = () => import(/* webpackChunkName: 'identity-verification' */ '../components/identity-verification/identity-verification.vue')
-const ImmediateRepaymentComponent = () => import(/* webpackChunkName: 'immediate-repayment' */ '../components/immediate-repayment/immediate-repayment.vue')
+const ImmediateRepaymentComponent = () => import(/* webpackChunkName: 'Immediate-repayment' */ '../components/Immediate-repayment/Immediate-repayment.vue')
 const IouDetailComponent = () => import(/* webpackChunkName: 'iou-detail' */ '../components/iou-detail/iou-detail.vue')
 const LendListComponent = () => import(/* webpackChunkName: 'lend-list' */ '../components/lend-list/lend-list.vue')
 const LendingInformationComponent = () => import(/* webpackChunkName: 'lending-information' */ '../components/lending-information/lending-information.vue')
@@ -204,8 +204,8 @@ export default new Router({
       component: IdentityVerificationComponent
     },
     {
-      path: '/immediate-repayment',
-      name: 'immediate-repayment',
+      path: '/Immediate-repayment',
+      name: 'Immediate-repayment',
       component: ImmediateRepaymentComponent
     },
     {
